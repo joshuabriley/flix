ab1fec8e56237d762e320c2da7598c1340a231db
Joshua Briley <josh@thebrileys.com>
2022-03-20 08:46:58 -0400

    Flop or cult classic logic

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -2,7 +2,7 @@ module MoviesHelper
 
   def total_gross(movie)
     if movie.flop?
-      'Flop!'
+      movie.reviews.count > 50 && movie.reviews.average(:stars).to_f >= 4.0 ? 'Cult Classic!' : 'Flop!'
     else
       number_to_currency(movie.total_gross, precision: 0)
     end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -19,6 +19,14 @@ class Movie < ApplicationRecord
   end
 
   def flop?
+    # Change the definition of the flop? method so that cult classics aren't included. For example,
+    # if a movie has more than 50 reviews and the average review is 4 stars or better, then the movie
+    # shouldn't be a flop regardless of the total gross.
+
+    # Here's a hint: Because the logic for determining whether a movie is a flop is tucked inside the Movie model,
+    # you can make this change in one place. When you can do that, you know you're on the right design path!
+
+
     total_gross.blank? || total_gross < 225_000_000
   end
 
