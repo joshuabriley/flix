6972e9b6d63f41ed08bd653c8a7e4338aabc89bd
Joshua Briley <josh@thebrileys.com>
2022-03-21 14:41:48 -0400

    User views.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,9 @@ class User < ApplicationRecord
   validates :email, presence: true,
                     format: { with: URI::MailTo::EMAIL_REGEXP },
                     uniqueness: { casesensitive: false }
-  validates :password, length: { minimum: 8, allow_blank: tue }
+  validates :password, length: { minimum: 8, allow_blank: true }
+
+  def gravatar_id
+    Digest::MD5::hexdigest(email.downcase)
+  end
 end
