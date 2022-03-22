d6ba9659c7ea758e51647df168bec6330ac086f5
Joshua Briley <josh@thebrileys.com>
2022-03-22 14:57:39 -0400

    Add session create method.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email_or_username]) ||
+           User.find_by(username: params[:email_or_username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, success: "Welcome back, #{user.name}!"
+    else
+      flash.now[:danger] = 'Invalid username/password combination'
+      render 'new'
+    end
   end
 
   def destroy
