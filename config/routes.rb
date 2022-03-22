3620bb3d8def91c8405c29e822f123525b390b53
Joshua Briley <josh@thebrileys.com>
2022-03-22 14:37:59 -0400

    Sign in controller and view.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
+  resource :session, only: [:new, :create, :destroy]
+  get 'signin' => 'sessions#new'
   resources :users
-  get "signup" => "users#new"
+  get 'signup' => 'users#new'
 end
