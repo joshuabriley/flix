2596bb5651da1c911c1823f20bc03b5774f28a22
Joshua Briley <josh@thebrileys.com>
2022-03-23 11:44:13 -0400

    Setup current user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user
+      session[:user_id] = @user.id
+      redirect_to @user, notice: 'Thanks for signing up!'
     else
       render 'new'
     end
