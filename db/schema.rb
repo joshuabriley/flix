febbd7b4a51872f538d5dbf5db4934c7b686a92d
Joshua Briley <josh@thebrileys.com>
2022-03-25 16:11:25 -0400

    Add fave and unfave functionality.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_24_185345) do
+ActiveRecord::Schema.define(version: 2022_03_25_162415) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "movie_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_favorites_on_movie_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -44,4 +53,6 @@ ActiveRecord::Schema.define(version: 2022_03_24_185345) do
     t.boolean "admin", default: false
   end
 
+  add_foreign_key "favorites", "movies"
+  add_foreign_key "favorites", "users"
 end
